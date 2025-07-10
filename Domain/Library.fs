fdaa2f295513b915b21c6e34f04daa67a5542d4c
Fernando <fernandodantas1996@gmail.com>
2025-07-10 20:40:30 -0300

    feat: first commit

---
+++ Domain/Library.fs
@@ -0,0 +1,50 @@
+﻿namespace Domain
+
+type Duration =
+    | WholeNote
+    | HalfNote
+    | QuarterNote
+    | EightNote
+    | SixteenthNote
+
+type NaturalNote =
+    | C
+    | D
+    | E
+    | F
+    | G
+    | A
+    | B
+
+type NoteAccident =
+    | Flat
+    | Natural
+    | Sharp
+
+type Note =
+    { NaturalNote: NaturalNote
+      NoteAccident: NoteAccident }
+
+type Octave = Octave of uint
+
+type Pitch = { Note: Note; Octave: Octave }
+
+type NoteEvent =
+    { Pitch: Pitch; NoteDuration: Duration }
+
+type ChordEvent =
+    { Root: Pitch; ChordDuration: Duration }
+
+type TimeSignature =
+    { Numerator: Duration
+      Denominator: Duration }
+
+type Measure =
+    { TimeSignature: TimeSignature
+      NoteEvents: List<NoteEvent>
+      ChordEvents: List<ChordEvent> }
+
+type Music = List<Measure>
+
+module XmlConverter =
+    ()
---
+++ UnitTests/UnitTest1.fs
@@ -0,0 +1,11 @@
+﻿module UnitTests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let Test1 () =
+    Assert.Pass()
