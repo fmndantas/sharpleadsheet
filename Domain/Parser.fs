a12a59122c57dee50bfa7bbd6077d78bd0759d21
Fernando <fernandodantas1996@gmail.com>
2025-07-18 15:42:33 -0300

    feat: implemented trivial passing code

--- Domain/Parser.fs
+++ Domain/Parser.fs
@@ -1,6 +1,36 @@
 module Domain.Parser
 
 open Domain.Types
+open Domain.MeasureBuilder
 
-let parse (fileContent: string) : Music = 
-    Music []
+let parse (fileContent: string) : Music =
+    Music
+        [ { Name = "Piano"
+            Measures =
+              [ aMeasure 1
+                |> withCNaturalKeySignature
+                |> withTimeSignature
+                    { Numerator = 2
+                      Denominator = Duration.HalfNote }
+                |> withNote
+                    { NoteName = NoteName.C
+                      Octave = 4
+                      Duration = Duration.EightNote }
+                |> withNote
+                    { NoteName = NoteName.D
+                      Octave = 4
+                      Duration = Duration.EightNote }
+                |> withNote
+                    { NoteName = NoteName.E
+                      Octave = 4
+                      Duration = Duration.EightNote }
+                |> withNote
+                    { NoteName = NoteName.D
+                      Octave = 4
+                      Duration = Duration.EightNote }
+
+                aMeasure 2
+                |> withCNaturalKeySignature
+                |> withTimeSignature
+                    { Numerator = 2
+                      Denominator = Duration.HalfNote } ] } ]
