ac33b1f3f94a9b06ba6d6f3133eca161acab031a
Fernando <fernandodantas1996@gmail.com>
2025-11-25 08:17:06 -0300

    feat: implemented Result.traverse

--- Domain/Result.fs
+++ Domain/Result.fs
@@ -7,3 +7,13 @@ let apply f r =
   | Ok _, Error es
   | Error es, Ok _ -> Error es
   | Error es1, Error es2 -> Error [ yield! es1; yield! es2 ]
+
+let traverse (rs: Result<'a, 'b list> list) : Result<'a list, 'b list> =
+  let folder r acc =
+    match r, acc with
+    | Ok v, Ok vs -> v :: vs |> Ok
+    | Ok _, Error es
+    | Error es, Ok _ -> Error es
+    | Error es1, Error es2 -> Error [ yield! es1; yield! es2 ]
+
+  List.foldBack folder rs (Ok [])
