6d0db3e078907658d3993fffc69c7fc5e1da6920
Fernando <fernandodantas1996@gmail.com>
2026-01-16 20:07:22 -0300

    refactor: made chord a modifier of rest

--- Domain/Rest.fs
+++ Domain/Rest.fs
@@ -3,10 +3,11 @@ module Domain.Rest
 type T = {
   Duration: Duration.T
   Modifiers: Modifier list
-  Chord: Chord.T option
 }
 
-and Modifier = Text of string
+and Modifier =
+  | Text of string
+  | Chord of Chord.T
 
 [<AutoOpen>]
 module private Helper =
@@ -15,24 +16,28 @@ module private Helper =
         Modifiers = m :: rest.Modifiers
   }
 
+  let maybeGetModifier<'a> (m: Modifier -> bool) (f: Modifier -> 'a option) (rest: T) : 'a option =
+    rest.Modifiers |> List.tryFind m |> Option.bind f
+
   let maybeAddModifier (m: Modifier option) (rest: T) : T =
     m |> Option.map (rest |> flip2 addModifier) |> Option.defaultValue rest
 
-let create (duration: Duration.T) : T = {
-  Duration = duration
-  Modifiers = []
-  Chord = None
-}
+let create (duration: Duration.T) : T = { Duration = duration; Modifiers = [] }
 
 let getDuration ({ Duration = duration }: T) : Duration.T = duration
 
-let getChord ({ Chord = chord }: T) = chord
+let getChord (rest: T) : Chord.T option =
+  rest
+  |> maybeGetModifier _.IsChord (function
+    | Chord c -> Some c
+    | _ -> None)
 
-let withChord (chord: Chord.T) (rest: T) : T = { rest with Chord = Some chord }
+let withChord (chord: Chord.T) (rest: T) : T = addModifier (Chord chord) rest
 
-let maybeWithChord (chord: Chord.T option) (rest: T) : T = { rest with Chord = chord }
+let maybeWithChord (chord: Chord.T option) (rest: T) : T =
+  maybeAddModifier (Option.map Chord chord) rest
 
-let withText (text: string) (rest: T) = addModifier (Text text) rest
+let withText (text: string) (rest: T) : T = addModifier (Text text) rest
 
 let maybeWithText (text: string option) (rest: T) : T =
   maybeAddModifier (Option.map Text text) rest
