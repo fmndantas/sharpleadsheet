55a2acc3ced73f85e560b3027720de9129a5bfc1
Fernando <fernandodantas1996@gmail.com>
2026-01-16 20:11:58 -0300

    refactor: made chord a modifier of note

--- Domain/Note.fs
+++ Domain/Note.fs
@@ -6,12 +6,12 @@ type T = {
   Pitch: Pitch.T
   Duration: Duration.T
   Modifiers: Modifier list
-  Chord: Chord.T option
 }
 
 and Modifier =
   | Tie
   | Text of string
+  | Chord of Chord.T
 
 [<AutoOpen>]
 module private Helper =
@@ -20,7 +20,6 @@ module private Helper =
     Pitch = Pitch.create noteName octave
     Duration = duration
     Modifiers = modifiers
-    Chord = None
   }
 
   let addModifier (m: Modifier) (note: T) : T = {
@@ -28,15 +27,17 @@ module private Helper =
         Modifiers = m :: note.Modifiers
   }
 
+  let maybeGetModifier<'a> (m: Modifier -> bool) (f: Modifier -> 'a option) (note: T) : 'a option =
+    note.Modifiers |> List.tryFind m |> Option.bind f
+
   let maybeAddModifier (m: Modifier option) (note: T) : T =
     m |> Option.map (note |> flip2 addModifier) |> Option.defaultValue note
 
 /// Create a note without modifiers
-let create octave noteName duration = {
+let create (octave: int) (noteName: NoteName.T) (duration: Duration.T) : T = {
   Pitch = Pitch.create noteName octave
   Duration = duration
   Modifiers = []
-  Chord = None
 }
 
 let create2 = create 2
@@ -49,22 +50,27 @@ let create5 = create 5
 
 let create6 = create 6
 
-let getPitch note = note.Pitch
+let getPitch (note: T) : Pitch.T = note.Pitch
 
-let getDuration note = note.Duration
+let getDuration (note: T) : Duration.T = note.Duration
 
-let getChord note = note.Chord
+let getChord (note: T) : Chord.T option =
+  note
+  |> maybeGetModifier _.IsChord (function
+    | Chord c -> Some c
+    | _ -> None)
 
-let isTied note = note.Modifiers |> List.contains Tie
+let isTied (note: T) : bool = note.Modifiers |> List.contains Tie
 
-let withChord (chord: Chord.T) (note: T) = { note with Chord = Some chord }
+let withChord (chord: Chord.T) (note: T) : T = addModifier (Chord chord) note
 
-let maybeWithChord (chord: Chord.T option) (note: T) = { note with Chord = chord }
+let maybeWithChord (chord: Chord.T option) (note: T) : T =
+  maybeAddModifier (Option.map Chord chord) note
 
-let withText (text: string) (note: T) = addModifier (Text text) note
+let withText (text: string) (note: T) : T = addModifier (Text text) note
 
-let maybeWithText (text: string option) (note: T) =
+let maybeWithText (text: string option) (note: T) : T =
   maybeAddModifier (Option.map Text text) note
 
-let maybeWithTie (tie: unit option) (note: T) =
+let maybeWithTie (tie: unit option) (note: T) : T =
   maybeAddModifier (Option.map (fun _ -> Tie) tie) note
