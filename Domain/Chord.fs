2a38cbb27f7e4ee97a8cdbb093ec3fe1ad8fff67
Fernando <fernandodantas1996@gmail.com>
2026-01-19 15:02:59 -0300

    refactor: adjusted get methods in chord module to conform with project standards

--- Domain/Chord.fs
+++ Domain/Chord.fs
@@ -12,7 +12,6 @@ let create (root: NoteName.T) (bass: NoteName.T option) (kind: string option) : 
   Kind = kind
 }
 
-// TODO: createWithRoot -> withRoot?
 let createWithRoot (root: NoteName.T) : T = create root None None
 
 let createWithKind (root: NoteName.T) (kind: string) : T = create root None (Some kind)
@@ -21,8 +20,8 @@ let createWithBass (root: NoteName.T) (bass: NoteName.T) : T = create root (Some
 
 let createWithBassAndKind (root: NoteName.T) (bass: NoteName.T) (kind: string) : T = create root (Some bass) (Some kind)
 
-let getRoot ({ Root = root }: T) = root
+let getRoot (chord: T) = chord.Root
 
-let getKind ({ Kind = kind }: T) = kind
+let getKind (chord: T) = chord.Kind
 
-let getBass ({ Bass = bass }: T) = bass
+let getBass (chord: T) = chord.Bass
